a1bc03adcf72bdab559063c2e838c79eb887a127
Justin Michalicek <jmichalicek@gmail.com>
2017-02-10 03:27:48 +0000

    add some command line stuff for python levenshtein

--- python/levenshtein.py
+++ python/levenshtein.py
@@ -1,11 +1,21 @@
 """
 Find the minimum edit distance between two words using the Levenshtein Distance in Python
 """
+from __future__ import print_function, unicode_literals, division, absolute_import
+
+# there are better options, but this is built in
+import argparse
 
 # These values are currently ignored
 REPLACE_COST = 2
 INSERT_COST = 1
 REMOVE_COST = 1
+
+
+class Implementation(object):
+    FULL = 'full'
+    SHORT_WORD = 'short-word'
+
 
 def init_matrix(word_one, word_two):
     """
@@ -30,7 +40,7 @@ def init_matrix(word_one, word_two):
     return distance_matrix
 
 
-def find_distance(word_one, word_two, print_matrix=False):
+def full_matrix(word_one, word_two, print_matrix=False):
     """
     Less efficient implementation which iterates over everything.
     You can easily see each step and it makes a nice printable matrix
@@ -39,6 +49,18 @@ def find_distance(word_one, word_two, print_matrix=False):
     This version first creates a base matrix, so iterates over each word once.
     Then iterates over each row in the matrix and then each column in that row
     comparing each character and updating the matrix.
+
+    Literally implements the basic algorithm
+    * initialize first row with word
+    * initialize first column with word_two
+    * for i in 0..word_one_len
+        for j in 0..word_two_len
+          if word_one[i] == word_two[j]
+            action_cost = 0 
+          else
+            action_cost = 1
+        matrix[i][j] = min(matrix[j-1], matrix[i-1], matrix[i-1][j-1]) + action_cost
+      return matrix[i][j]
     """
     distance = 0
     word_one_len = len(word_one)
@@ -91,7 +113,7 @@ def find_distance(word_one, word_two, print_matrix=False):
     return current_cost
 
 
-def find_distance_optimized(word_one, word_two, print_matrix=False):
+def shortest_word(word_one, word_two, *args, **kwargs):
     """
     Slightly optimized implementation
 
@@ -138,20 +160,25 @@ def find_distance_optimized(word_one, word_two, print_matrix=False):
         distance_matrix.append(row)
 
     distance += abs(word_one_len - word_two_len)
-
-    if print_matrix:
-        # it will be weird and not make much sense
-        print([' ', ' '] + list(word_one))
-        for idx, x in enumerate(distance_matrix):
-            if idx > 0:
-                line = [word_two[idx-1]]
-            else:
-                line = [' ']
-            line += [str(y) for y in x]
-            print(line)
-
     return distance
 
 
 if __name__ == "__main__":
-    print(find_distance_optimized('cows', 'cow', True))
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    # should be able to use metavar=('word1', 'word2') but current python I am using
+    # does not seem to like it
+    parser.add_argument('words', nargs=2,
+                        help='The two words to compare')
+    parser.add_argument('-i', '--implementation', dest='implementation',
+                        default=Implementation.FULL,
+                        choices=[Implementation.FULL, Implementation.SHORT_WORD],
+                        help='The implementation of levenshtein to use')
+
+    args = parser.parse_args()
+    words = args.words
+    if args.implementation == Implementation.FULL:
+        lev_func = full_matrix
+    elif args.implementation == Implementation.SHORT_WORD:
+        lev_func = shortest_word
+
+    print(lev_func(*words))
